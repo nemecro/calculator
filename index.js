460abc1ec6ca68760ab1e41786f6b21c2e04ef61
nemecro <nemecstudies@gmail.com>
2024-10-12 16:43:15 +0200

    feat: fix the behaviour of negative sign on second operand

--- index.js
+++ index.js
@@ -28,16 +28,12 @@ const operate = function(num1, operator, num2){
     switch (operator){
         case '+': 
             return add(num1, num2);
-            break;
         case '-':
             return subtract(num1, num2);
-            break;
         case '*':
             return multiply(num1, num2);
-            break;
         case '/':
             return divide(num1, num2);
-            break;
         default:
             return 'invalid operator';
     }
@@ -51,9 +47,12 @@ const reset = function(){
     decimalPressed = false;
     firstOperator = true;
     negative = false;
+    negativeFirst = true;
 }
 // check if the negative sign was applied to the number
 let negative = false;
+// setting the first negative
+let negativeFirst = true;
 // check if the first operator was pressed
 let firstOperator = true;
 // check if the decimal was pressed
@@ -104,8 +103,16 @@ buttons.forEach(button => {
         if (button.textContent == '+ / -'){
             //
             if (negative == false){
-                calculatorDisplay.textContent = '-' + calculatorDisplay.textContent;
-                negative = true;
+                // if setting the first negative
+                if (negativeFirst){
+                    calculatorDisplay.textContent = '-' + calculatorDisplay.textContent;
+                    negative = true;
+                // if setting the second negative
+                } else {
+                    let displayArr = calculatorDisplay.textContent.split(' ');
+                    displayArr[displayArr.length - 1] = `(${displayArr[displayArr.length - 1] * (-1)})`;
+                    calculatorDisplay.textContent = displayArr.join(' ');
+                }
             } else {
                 calculatorDisplay.textContent = calculatorDisplay.textContent.substring(1, calculatorDisplay.textContent.length)
                 negative = false;
@@ -114,7 +121,6 @@ buttons.forEach(button => {
 
         // OPERATORs section
         if (button.classList.contains('operator')){
-            let arrFirstPart;
             // CHECK IF IT IS THE FIRST
             if (firstOperator){
                 // add the space here so I can split it into an array
@@ -122,12 +128,17 @@ buttons.forEach(button => {
                 cleared = false;
                 firstOperator = false;
                 calculatorDisplay.textContent += ` ${button.textContent} `;
-                arrFirstPart = calculatorDisplay.textContent.split(' ');
+                negativeFirst = false;
+            } else {
+                let arrFirstPart = calculatorDisplay.textContent.split(' ');
                 number1 = arrFirstPart[0];
                 operator = arrFirstPart[1];
-            } else {
                 let arrSecondPart = calculatorDisplay.textContent.split(' ');
                 number2 = arrSecondPart.slice(arrSecondPart.indexOf(operator) + 1)[0];
+                if (number2.includes('(')){
+                    number2 = number2.replace('(', '');
+                    number2 = number2.replace(')', '');
+                }
                 console.log(`${number1} ${operator} ${number2}`);
                 let result = operate(number1, operator, number2);
                 reset();
@@ -143,8 +154,6 @@ buttons.forEach(button => {
                         number1 = arrFirstPart[0];
                         operator = arrFirstPart[1];
                 }
-                // MAKE THE CALCULATION
-
             }
         }
 
